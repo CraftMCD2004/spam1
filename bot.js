de51d49cd56b417f9e67b6325d2605b8d1a39bb4
CraftMCD2004 <45973894+CraftMCD2004@users.noreply.github.com>
2018-12-24 21:38:41 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,4 +31,4 @@ if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('
  });
 
  
-client.login('NTI0OTI2Mjc4MDY3ODE0NDQw.DvvOJQ.JejMo2ezyeX8yS6yzOaMWN00S9k');
+client.login('NTI0OTI5OTQ0OTE3MjQ1OTUy.DvvO9A.wysAUx-D8EM-qKYGR1i80n1hrQM');
